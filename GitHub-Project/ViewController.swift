2bd72c4fc02db502fe4a1940e742314a74d4b31c
Arshad Shaik <arshad.iosdeveloper@gmail.com>
2025-10-23 16:10:23 +0530

    Another print

--- GitHub-Project/ViewController.swift
+++ GitHub-Project/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Loaded only once")
+        print("Trying for merge conflict")
     }
 
 
