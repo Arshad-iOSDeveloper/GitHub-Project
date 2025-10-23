32742f2a09eb388601d2870c7fcf381b85475211
Arshad Shaik <arshad.iosdeveloper@gmail.com>
2025-10-23 15:54:23 +0530

    First Commit

--- GitHub-Project/AppDelegate.swift
+++ GitHub-Project/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("This is called on App Launch")
         return true
     }
 
